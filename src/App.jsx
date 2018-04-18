92386705d8785ed2cf66413505bf999baf045f82
Zamarise Montoya <zamarise@gmail.com>
2018-04-17 21:24:19 -0700

    Add code for basic usability and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-
   render() {
     return (
       <div className='container'>
-
+        <h1 className='text-center'>VSTDA</h1>
       </div>
     );
   }
