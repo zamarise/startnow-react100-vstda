2a4a130d36da993fced333a7dd2ecbc55a9240df
Zamarise Montoya <zamarise@gmail.com>
2018-04-18 18:12:51 -0700

    Add more code

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,72 @@ class App extends Component {
     return (
       <div className='container'>
         <h1 className='text-center'>VSTDA</h1>
+        <p className='lead bg-light text-gray-dark text-center'>
+          A very simple to-do app made in React
+        </p>
+
+        <hr />
+
+        <div className='row'>
+          <div className='col-sm-4'>
+            <div className='panel panel-default'>
+              <div className='panel-heading panel-title'>Add New To-do</div>
+              <p>
+                <div className='panel-body'>
+                  I want to...
+                  <input
+                    name='amountDue'
+                    className='form-control'
+                    // value={ this.state. }
+                    // onChange={ this. }
+                    placeholder='Enter amount due'
+                    type='number'
+                  />
+                </div>
+                <div className='panel-body'>
+                  <div className='dropdown'>
+                    <button
+                      className='btn btn-default dropdown-toggle'
+                      type='button'
+                      id='dropdownMenu1'
+                      data-toggle='dropdown'
+                      aria-haspopup='true'
+                      aria-expanded='true'
+                    >
+                      Dropdown
+                      <span className='caret' />
+                    </button>
+                    <ul className='dropdown-menu' aria-labelledby='dropdownMenu1'>
+                      <li>
+                        <a href='#'>Action</a>
+                      </li>
+                      <li>
+                        <a href='#'>Another action</a>
+                      </li>
+                      <li>
+                        <a href='#'>Something else here</a>
+                      </li>
+                      <li role='separator' className='divider' />
+                      <li>
+                        <a href='#'>Separated link</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </p>
+
+              <div className='panel-footer'>
+                <button
+                  className='btn btn-default btn-block'
+                  type='button'
+                  // onClick={ this. }
+                >
+                  Add
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
